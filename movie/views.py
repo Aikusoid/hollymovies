0b0ebe35f2e7a67ebe2e848eb9efccf9d26044c4
Aigerim <aigerim.adrysheva@gmail.com>
2021-11-27 13:14:33 +0100

    adding new templates: movie detail, genre deetail, data types testing

--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url, get_object_or_404
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
 
@@ -21,3 +21,37 @@ def movie_list_view(request):
 
     }
     return TemplateResponse(request, 'movieList.html', context=context)
+
+
+def movie_detail_view(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+
+    context = {
+        'movie': movie,
+    }
+    return TemplateResponse(request, 'movieDetail.html', context=context)
+
+
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+
+    context = {
+        'movie': genre,
+    }
+    return TemplateResponse(request, 'genreDetail.html', context=context)
+
+
+def testing_cheatsheet_view(request):
+    # python list[0]
+    # template language jinja2 list.0
+
+    # python dict['key']
+    # template language jinja2 dict.key
+    context = {
+        'list': ['index0', 'index1'],
+        'dict': {
+            'key': 'value',
+            'key2': 'value2',
+        }
+    }
+    return TemplateResponse(request, 'dataTypesTesting.html', context=context)
